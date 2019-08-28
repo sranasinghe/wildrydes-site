ef92096844b08b4842255c22bec5c1e0726215eb
sranasinghe <sranasinghe@gmail.com>
2019-08-28 17:45:32 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_l3KU0R4DZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6auoahjqg0q6oi86tn3rgml4dm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
